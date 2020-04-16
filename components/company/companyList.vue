098101716d3fa147d37383a6e2d485e9767c805e
swaty007 <swaty0007@gmail.com>
2020-04-16 03:23:14 +0300

    filters add + cols

--- components/company/companyList.vue
+++ components/company/companyList.vue
@@ -1,33 +1,61 @@
 <template>
     <div v-if="items">
-        <v-col
-                cols="12"
-                md="12">
-        <v-hover
-                v-for="(item, i) in items"
-                :key="i"
-                 v-slot:default="{ hover }">
-        <v-card
-                :hover="true"
-                :light="true"
-                :outlined="true"
-                :elevation="hover ? 12 : 2"
-                class="text-left mb-12 pa-3 transition-swing">
-            <CompanyListItem :item="item" :btn="true" />
-        </v-card>
-        </v-hover>
-        <client-only>
-            <infinite-loading
-                    v-if="items.length"
-                    spinner="waveDots"
-                    :distance="200"
-                    @infinite="infiniteHandler">
-                <span slot="no-results"></span>
-                <span slot="no-more"></span>
-                <span slot="error"></span>
-            </infinite-loading>
-        </client-only>
-        </v-col>
+        <v-container fluid>
+            <v-row>
+                <v-col
+                        cols="12"
+                        md="4">
+                    <v-text-field label="ЕДРПОУ" outlined/>
+                </v-col>
+                <v-col
+                        cols="12"
+                        md="4">
+                    <v-select label="КВЕД" outlined/>
+                </v-col>
+                <v-col
+                        cols="12"
+                        md="4">
+                    <v-select label="Городу/Области" outlined/>
+                </v-col>
+                <v-col
+                        cols="12"
+                        md="4">
+                    <v-select label="Статус" outlined/>
+                </v-col>
+            </v-row>
+        </v-container>
+        <v-container>
+            <v-row>
+                <v-col
+                        v-for="(item, i) in items"
+                        :key="i"
+                        cols="12"
+                        md="6">
+                    <v-hover
+                            v-slot:default="{ hover }">
+                        <v-card
+                                :hover="true"
+                                :light="true"
+                                :outlined="true"
+                                :elevation="hover ? 12 : 2"
+                                class="text-left ma-0 mb-12 pa-3 transition-swing">
+                            <CompanyListItem :item="item" :btn="true"/>
+                        </v-card>
+                    </v-hover>
+                </v-col>
+            </v-row>
+        </v-container>
+            <client-only>
+                <infinite-loading
+                        v-if="items.length"
+                        spinner="waveDots"
+                        :distance="200"
+                        @infinite="infiniteHandler">
+                    <span slot="no-results"></span>
+                    <span slot="no-more"></span>
+                    <span slot="error"></span>
+                </infinite-loading>
+            </client-only>
     </div>
 </template>
 <script>
--- components/company/companyListItem.vue
+++ components/company/companyListItem.vue
@@ -5,11 +5,11 @@
                 <template v-if="typeof (value) === 'object'">
                     <p v-if="value" class="black--text body-2 mb-0"><strong>{{ key }}:</strong></p>
                     <div v-for="(val, k, ind) in value" :key="ind" class="black--text">
-                        <template v-if="typeof (val) === 'object'">
+                        <div v-if="typeof (val) === 'object'" class="mb-1">
                             <div v-for="(val2, k2, ind2) in val" :key="ind2" class="black--text mb-0 caption ml-5">
                                 <strong>{{ k2 }}</strong> {{ val2 }}
                             </div>
-                        </template>
+                        </div>
                         <div v-else-if="val" class="caption ml-5"><strong>{{ k }}:</strong> {{ val }}</div>
                     </div>
                 </template>
